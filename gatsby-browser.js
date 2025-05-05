bf20d978214ee396e43eb0c7d77f123294405c49
KhawerAbbas7 <85282172+KhawerAbbas7@users.noreply.github.com>
2025-05-05 15:14:30 +0500

    update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,6 @@
 import './src/css/index.css';
-// gatsby-ssr.js and gatsby-browser.js
 import React from 'react';
 
 export const onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes({ className: 'light' }); // default
+  setHtmlAttributes({ className: 'light' });
 };
